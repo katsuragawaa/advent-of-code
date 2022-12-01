ebea98b09447db830706619a1f64323925a1138f
katsuragawa <andre.katsuragawa@gmail.com>
2022-12-01 07:29:19 -0300

    setup aoc

---
+++ 22_rust/src/lib.rs
@@ -0,0 +1,15 @@
+//! # Advent of Code 2022 Solutions in Rust
+//!
+//! - ❔ [About Advent of Code](https://adventofcode.com/about)
+//! - 📆 [List of Problems](https://adventofcode.com/2022)
+
+
+// #[allow(unused_imports)]
+#[macro_use]
+extern crate aoc_runner_derive;
+extern crate aoc_runner;
+
+mod day1;
+
+aoc_lib! { year = 2022 }
+
